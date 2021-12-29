c1894db01e5d01f4409f705003c828cf7148dd4d
Aoran Wu <awu48@wisc.edu>
2021-12-29 07:18:33 +0000

    make block_num customizable

--- get_roofline.py
+++ get_roofline.py
@@ -1,18 +1,22 @@
 import os
 import csv
-op_per_byte = 1
+import sys
+op_per_byte = 1 # more accurately, op per block
 repeated = 100
-output_filename = "roofline_server.csv"
+block_num = 8 # one block is 16B
+platform = "server"
 res = [['op_per_byte', 'op_per_second']]
-for op_per_byte in list(range(1, 200)) + [300, 400, 500, 1000, 5000, 10000, 50000, 100000]:
-# for op_per_byte in [1, 10, 100, 1000, 10000, 100000, 1000000]:
-    total = 0
-    for i in range(repeated):
-        total += float(os.popen("./get_roofline "+str(op_per_byte)).read().split()[1].split(":")[1])
-    print("op_per_byte:", op_per_byte)
-    print("op_per_second:", total/repeated)
-    res.append([op_per_byte, total/repeated])
-with open(output_filename, "w") as csvfile:
-    writer = csv.writer(csvfile)
-    for data in res:
-        writer.writerow(data)
+for block_num in [1,4,8]:
+    output_filename = "roofline_"+platform+"_"+str(block_num)+".csv"
+    for op_per_byte in list(range(1, 200)) + [300, 400, 500, 1000, 5000, 10000, 50000, 100000]:
+    # for op_per_byte in [1, 10, 100, 1000, 10000, 100000, 1000000]:
+        total = 0
+        for i in range(repeated):
+            total += float(os.popen("./get_roofline "+str(op_per_byte)+" "+str(block_num)).read().split()[1].split(":")[1])
+        print("op_per_byte:", op_per_byte)
+        print("op_per_second:", total/repeated)
+        res.append([op_per_byte, total/repeated])
+    with open(output_filename, "w") as csvfile:
+        writer = csv.writer(csvfile)
+        for data in res:
+            writer.writerow(data)
